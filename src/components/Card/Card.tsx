7070bb3b5a5239d71b98b1e37a60875a70447aaa
Adam Olivegren <adam@olivegren.se>
2023-06-30 14:10:47 +0200

    added styles to components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import {
+  BsFillArrowUpCircleFill,
+  BsFillArrowDownCircleFill,
+} from "react-icons/bs";
 import { UserType } from "../User/User";
 import { CommentsFeedType } from "../CommentsFeed/CommentsFeed";
 import "./Card.css";
+import { useState } from "react";
 
 export type CardType = {
   title: string;
@@ -19,14 +23,37 @@ export function Card({
   comments,
   numberOfComments,
 }: CardType) {
+  const [isUpvoted, setIsUpvoted] = useState<boolean>(false);
+  const [isDownvoted, setIsDownvoted] = useState<boolean>(false);
+
+  const handleVoteClick = (voteType: string) => {
+    setIsUpvoted(voteType === "up" && !isUpvoted);
+    setIsDownvoted(voteType === "down" && !isDownvoted);
+  };
+
   return (
     <div className="card-div">
-      <h2>{title}</h2>
-      <img src={img} alt="" className="src" />
-      <div className="card-info">
-        <p>Posted by: {author?.name}</p>
-        <p>{timeSincePost} hours ago</p>
-        <p>Number of comments: {numberOfComments}</p>
+      <div className="left-div">
+        <BsFillArrowUpCircleFill
+          style={{ color: isUpvoted ? "green" : "" }}
+          onClick={() => handleVoteClick("up")}
+        />
+        <p style={{ color: isUpvoted ? "green" : isDownvoted ? "red" : "" }}>
+          23
+        </p>
+        <BsFillArrowDownCircleFill
+          style={{ color: isDownvoted ? "red" : "" }}
+          onClick={() => handleVoteClick("down")}
+        />
+      </div>
+      <div className="right-div">
+        <h2>{title}</h2>
+        <img src={img} alt="" className="src" />
+        <div className="card-info">
+          <p>Posted by: {author?.name}</p>
+          <p>{timeSincePost} hours ago</p>
+          <p>Number of comments: {numberOfComments}</p>
+        </div>
       </div>
     </div>
   );
