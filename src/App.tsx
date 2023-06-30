de9106a06ddd662ea09a8646e81790da42d2d52e
Adam Olivegren <adam@olivegren.se>
2023-06-30 10:56:54 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Feed } from "./components/Feed/Feed";
-import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Feed />
+      // TODO: subreddits
     </div>
   );
 }
